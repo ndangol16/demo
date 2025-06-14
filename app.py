54b206dce21f481a6f571d6b07ae6d18cdda6193
nirnaya dangol <dnirnaya@gmail.com>
2025-06-15 03:12:56 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Hello from Flask on port 5000!</h1>"
+
+@app.route('/hello')
+def helloagain():
+    return "<h1>Hello endpoint!</h1>"
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
