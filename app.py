c04a8c08d36dcb17193b390b45184c58ce6d98c9
nirnaya dangol <dnirnaya@gmail.com>
2025-06-15 04:13:48 +0545

    bye endpoint added

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def hello():
 def helloagain():
     return "<h1>Hello endpoint!</h1>"
 
+@app.route('/bye')
+def helloagain():
+    return "<h1>Bye!</h1>"
+
 if __name__ == '__main__':
     app.run(port=5000)
 
