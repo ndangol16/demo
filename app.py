a103b5552eb0f17904ce437ad38ed126b28cdaff
nirnaya dangol <dnirnaya@gmail.com>
2025-06-15 05:30:23 +0545

    lint found error fix v2

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return "<h1>Hello from Flask on port 5000!</h1>"
+
 
 @app.route('/hello')
 def helloagain():
     return "<h1>Hello endpoint!</h1>"
 
+
 @app.route('/bye')
 def bye():
     return "<h1>Bye!</h1>"
+
 
 @app.route('/byeagain')
 def byeagain():
     return "<h1>Bye again!</h1>"
 
+
 if __name__ == '__main__':
     app.run(port=5000)
-
